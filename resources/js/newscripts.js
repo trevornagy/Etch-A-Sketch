8da586ac214314f23969162ce870ce8dc83af9b4
Trevor Nagy <tennagy@hotmail.com>
2016-07-23 20:10:25 -0400

    Finished project

--- resources/js/newscripts.js
+++ resources/js/newscripts.js
@@ -1,10 +1,9 @@
 $('#OptionsSubmit').on('click', function()
 {
 	gridCheck();
-	return false;
 });
 
-// Check page for existing page
+// Check page for existing grid
 function gridCheck()
 {
 	if ($('#content').hasClass('onPage'))
@@ -16,7 +15,7 @@ function gridCheck()
 }
 
 
-// Verify that the values inputted are within the range that we allow
+// Verify that the user input is within the range that we allow
 function gridVerify()
 {
 	var dimensions = $('#dimensions').val();
@@ -56,7 +55,33 @@ function gridCreate(dimensions)
 	}
 }
 
-$('.square').mouseenter(function() 
+
+// Check if mouse has entered the square
+$('body').on('mouseenter', 'div .square', function()
 {
-	$('.square').css('background', 'red');	
+	// Get colour value from select box
+	if ($('select[name=colSelection]').val() == 'black')
+	{
+		$(this).css('background-color', '#000000');
+	}
+	else if ($('select[name=colSelection]').val() == 'red')
+	{
+		$(this).css('background-color', '#ff0000');
+	}
+	else if ($('select[name=colSelection]').val() == 'blue')
+	{
+		$(this).css('background-color', '#0000FF');
+	}
+	else 
+	{
+		/* Random color generator thanks to Stackoverflow
+		   http://stackoverflow.com/a/5365036            */
+		$(this).css('background-color', "#"+((1<<24)*Math.random()|0).toString(16));
+	}
+});
+
+// Reset grid back to white
+$('#OptionsReset').on('click', function()
+{
+	$('div .square').css('background-color', '#FFFFFF');
 });
